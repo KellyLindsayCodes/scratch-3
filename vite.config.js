adf4f87eb028c9eedec4dd4527b2479956ab52a7
Kelly Lindsay <kmlindsay1994@gmail.com>
2023-06-19 06:59:19 +1000

    Update link references

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default {
-    base: '/scratch-3/', // Replace 'my-project' with your GitHub repository name
-  };
-  
+  // ...other configuration options
+  base: '/scratch-3/', // Replace 'scratch-3' with your repository name
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'pages/about/about.html'),
+        contact: resolve(__dirname, 'pages/contact/contact.html'),
+        projects: resolve(__dirname, 'pages/projects/projects.html'),
+      },
+    },
+  },
+};
